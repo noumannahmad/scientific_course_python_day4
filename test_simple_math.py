26ce7d3693e068b799e4ad7aaccc74df585a25ad
Nouman Ahmad <43134572+noumannahmad@users.noreply.github.com>
2023-03-09 15:14:00 +0100

    Update test_simple_math.py

--- test_simple_math.py
+++ test_simple_math.py
@@ -1,1 +1,31 @@
-#!/usr/bin/env python3# -*- coding: utf-8 -*-"""Created on Thu Mar  9 14:01:02 2023@author: noumanahmad"""# test_simple_math.pyimport simple_mathdef test_simple_add():    assert simple_math.simple_add(2, 3) == 5  def test_simple_sub():    assert simple_math.simple_sub(5, 3) == 2def test_simple_mult():    assert simple_math.simple_mult(2, 3) == 6def test_simple_div():    assert simple_math.simple_div(6, 3) == 2def test_poly_first():    assert simple_math.poly_first(2, 3, 4) == 11def test_poly_second():    assert simple_math.poly_second(2, 3, 4, 5) == 31print("Successfully run")
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  9 14:01:02 2023
+
+@author: noumanahmad
+"""
+
+# test_simple_math.py
+
+import simple_math
+
+def test_simple_add():
+    assert simple_math.simple_add(2, 3) == 5
+  
+def test_simple_sub():
+    assert simple_math.simple_sub(5, 3) == 2
+
+def test_simple_mult():
+    assert simple_math.simple_mult(2, 3) == 6
+
+def test_simple_div():
+    assert simple_math.simple_div(6, 3) == 2
+
+def test_poly_first():
+    assert simple_math.poly_first(2, 3, 4) == 11
+
+def test_poly_second():
+    assert simple_math.poly_second(2, 3, 4, 5) == 31
+
+print("Successfully run")
